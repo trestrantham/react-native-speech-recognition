f8473cd68bf7cecdf6b328886867fcb7551b672e
Tres Trantham <tres@trestrantham.com>
2017-01-31 17:27:55 -0500

    Add speech timeout; clean up

--- ios/Speech/RNSRSpeechToText.m
+++ ios/Speech/RNSRSpeechToText.m
@@ -7,18 +7,24 @@
 
 @interface RNSRSpeechToText() <SFSpeechRecognitionTaskDelegate,SFSpeechRecognizerDelegate>
 @property (strong, nonatomic) RCTEventDispatcher* eventDispatcher;
+@property (strong, nonatomic) NSTimer* speechTimeoutTimer;
 
 @property (strong, nonatomic) AVAudioEngine* audioEngine;
 @property (strong, nonatomic) AVSpeechSynthesizer* speechSynthesizer;
 @property (strong, nonatomic) SFSpeechAudioBufferRecognitionRequest* recognitionRequest;
-@property (strong, nonatomic) SFSpeechRecognizer* speechRecognizer;
-
 @property (strong, nonatomic) SFSpeechRecognitionTask* recognitionTask;
+@property (strong, nonatomic) SFSpeechRecognizer* speechRecognizer;
 
 @property (copy) void (^completionHandler)(NSString *);
 @end
 
 @implementation RNSRSpeechToText
+
+static int const SPEECH_TIMEOUT_SECONDS = 45;
+static double const POWER_MIN = -25;
+static double const POWER_MAX = -80;
+static double const POWER_SCALE_FACTOR = (1 - 0) + 0; // (MAX - MIN) + MIN;
+static BOOL const logging = false;
 
 - (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher
 {
@@ -65,21 +71,15 @@
   AVAudioInputNode *inputNode = [self.audioEngine inputNode];
 
   if (self.recognitionRequest == nil) {
-    NSLog(@"Unable to created a SFSpeechAudioBufferRecognitionRequest object");
+    logging && NSLog(@"Unable to created a SFSpeechAudioBufferRecognitionRequest object");
   }
 
   if (inputNode == nil) {
-    NSLog(@"Unable to created a inputNode object");
+    logging && NSLog(@"Unable to created a inputNode object");
   }
 
-  // self.recognitionRequest.shouldReportPartialResults = true;
   self.recognitionRequest.taskHint = SFSpeechRecognitionTaskHintDictation;
-
   self.recognitionTask = [self.speechRecognizer recognitionTaskWithRequest:self.recognitionRequest delegate:self];
-
-  double POWER_MIN = -25;
-  double POWER_MAX = -80;
-  double POWER_SCALE_FACTOR = (1 - 0) + 0; // (MAX - MIN) + MIN;
 
   [inputNode installTapOnBus:0
     bufferSize:4096
@@ -101,7 +101,6 @@
         Float32 normalizedValue = (averageValue == 0) ? -100 : 20.0;
         Float32 powerValue = normalizedValue * log10f(averageValue);
 
-        NSLog(@"%f", normalizedValue * log10f(averageValue));
         averagePower = 1 - ((powerValue - POWER_MIN) / (POWER_MAX - POWER_MIN)) * POWER_SCALE_FACTOR;
         averagePower = averagePower < 0 ? 0 : averagePower;
         averagePower = averagePower > 1 ? 1 : averagePower;
@@ -111,14 +110,27 @@
     }
   ];
 
+  // Speech recognition can only run for ~1 minute so we set a timer to return back to the caller
+  // when the timeout is reached. This allows a caller to respond appropriately (in the view, etc.).
+  self.speechTimeoutTimer = [NSTimer
+    scheduledTimerWithTimeInterval:SPEECH_TIMEOUT_SECONDS
+    target: self
+    selector: @selector(triggerSpeechTimeout:)
+    userInfo: nil
+    repeats: false
+  ];
+
   [self.audioEngine prepare];
   [self.audioEngine startAndReturnError:&outError];
 
-  NSLog(@"Error %@", outError);
+  logging && NSLog(@"Error %@", outError);
 }
 
 - (void)stop:(void (^)(NSString *))completionHandler
 {
+  [self.speechTimeoutTimer invalidate];
+  self.speechTimeoutTimer = nil;
+
   if (self.audioEngine.isRunning) {
     [self.audioEngine stop];
     [[self.audioEngine inputNode] removeTapOnBus:0];
@@ -126,19 +138,17 @@
     if (self.recognitionRequest != nil) {
       [self.recognitionRequest endAudio];
     }
-
   }
 
   self.recognitionRequest = nil;
   self.recognitionTask = nil;
 
   [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceListening" body:@(false)];
-  [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceProcessing" body:@(false)];
 }
 
  - (void)speechRecognitionTask:(SFSpeechRecognitionTask *)task didFinishRecognition:(SFSpeechRecognitionResult *)result
 {
-  NSLog(@"speechRecognitionTask:(SFSpeechRecognitionTask *)task didFinishRecognition");
+  logging && NSLog(@"speechRecognitionTask:(SFSpeechRecognitionTask *)task didFinishRecognition");
   NSString *translatedString = [[[result bestTranscription] formattedString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 
   if ([result isFinal]) {
@@ -150,15 +160,13 @@
 
 - (void)speechRecognitionTaskWasCancelled:(SFSpeechRecognitionTask *)task
 {
-  NSLog(@"speechRecognitionTaskWasCancelled");
+  logging && NSLog(@"speechRecognitionTaskWasCancelled");
   [self stop:false];
-  [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceListening" body:@(false)];
-  [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceProcessing" body:@(false)];
 }
 
 - (void)speechRecognitionTask:(SFSpeechRecognitionTask *)task didHypothesizeTranscription:(SFTranscription *)transcription
 {
-  NSLog(@"speechRecognitionTask didHypothesizeTranscription");
+  logging && NSLog(@"speechRecognitionTask didHypothesizeTranscription");
   NSString *translatedString = [[transcription formattedString] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 
   [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:translatedText" body:@{@"text": translatedString, @"state": @"hypothesized"}];
@@ -166,8 +174,15 @@
 
 - (void)speechRecognizer:(SFSpeechRecognizer *)speechRecognizer availabilityDidChange:(BOOL)available
 {
-  NSLog(@"speechRecognizer availabilityDidChange");
+  logging && NSLog(@"speechRecognizer availabilityDidChange");
   [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceAvailable" body:@(available)];
+}
+
+- (void)triggerSpeechTimeout:(NSTimer *)timer
+{
+  logging && NSLog(@"triggerSpeechTimeout");
+  [self stop:false];
+  [self.eventDispatcher sendAppEventWithName:@"RNSpeechRecognition:voiceSpeechTimeout" body:@(true)];
 }
 
 @end
